538779ae89e1a892fbafcd4de47753f9bf65cb63
RogerJackD <rogerzapata114@gmail.com>
2026-02-06 23:39:24 -0500

    feat(services): add method POST createProduct

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -27,6 +27,33 @@ export const ProductService = {
 
         return await response.json();
 
+    },
+    async CreateProduct(productData: Product): Promise<Product[]>{
+        
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/products`,{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(productData)
+        });
+
+        if( !response.ok ) {
+            let errorMessage = `Error ${response.status}: ${response.statusText}`;
+            
+            try {
+                const errorData = await response.json();
+                errorMessage = errorData.message || errorData;
+
+            } catch {
+               console.log("error unexpected, check servers logs");
+            }
+
+            throw new Error(errorMessage);
+        };
+
+        return await response.json();
+
     }
 
 };
